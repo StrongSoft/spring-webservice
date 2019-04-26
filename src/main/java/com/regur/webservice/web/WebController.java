f548d0398bb1006b60973a49e708719d1875eedd
Lee, Seungmin <regur007@gmail.com>
2019-04-26 16:49:08 +0900

    first commit

--- src/main/java/com/regur/webservice/web/WebController.java
+++ src/main/java/com/regur/webservice/web/WebController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @AllArgsConstructor
 public class WebController {
+
     private PostsService postsService;
 
     @GetMapping("/")
