47b96f3dd7a922a4e3978c50123c8b4b2553c9b6
Lee, Seungmin <regur007@gmail.com>
2019-04-26 16:47:49 +0900

    first commit

--- src/main/java/com/regur/webservice/web/WebRestController.java
+++ src/main/java/com/regur/webservice/web/WebRestController.java
@@ -1,16 +1,30 @@
 package com.regur.webservice.web;
 
+import com.regur.webservice.domain.posts.PostsRepository;
+import com.regur.webservice.dto.posts.PostsSaveRequestDto;
+import com.regur.webservice.service.PostsService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author leeseungmin on 2019-04-25
  */
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsService.save(dto);
     }
 }
